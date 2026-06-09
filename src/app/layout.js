81f3d0ad6a1805a6fb585bbc4f32bd3e80632eb7
pcle1202 <pcle1202@gmail.com>
2026-06-09 12:01:34 -0400

    added vercel speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { poppins } from "./fonts";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Cafe 104',
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
 
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
